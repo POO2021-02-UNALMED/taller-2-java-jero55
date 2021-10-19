a483a9aeb6f46f023e94e966b9e797e6936bf32d
jero55 <jsalazars@unal.edu.co>
2021-10-19 16:58:17 -0500

    Create Asiento.java

---
+++ src/test/java/test/Asiento.java
@@ -0,0 +1,15 @@
+package test;
+public class Asiento {
+
+	String color;
+	int precio;
+	int registro;
+	
+	void cambiarColor(String color) {
+		if (color== "verde"||color =="amarillo"||color=="negro"||color=="blanco"||color=="rojo") {
+			
+			this.color = color;
+		
+		}
+	}
+}
