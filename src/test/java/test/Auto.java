0afee4cbdb34eac1d1f9bcab8b2da0af570f04a9
jero55 <jsalazars@unal.edu.co>
2021-10-19 17:27:09 -0500

    Update Auto.java

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -19,18 +19,22 @@ public class Auto {
 		return count;
 	}
 	
-	String verificarIntegridad() {
-		if (this.registro != this.motor.registro) {
+	String verificarintegridad() {
+		if (this.registro != this.motor.registro) 
 			return "Las piezas no son originales";
-		}
-		if (this.registro == this.motor.registro) {
-			for (int i = 0; i < asientos.length; i++) {
-				if (asientos[i] != null) {
-					if (asientos[i].registro != this.registro)
+		
+		if (this.registro == this.motor.registro) 
+			for (int j = 0;j<asientos.length;j++) {
+				if (asientos[j] != null) {
+					if (asientos[j].registro != this.registro) {
 						return "Las piezas no son originales";
+					}
 				}
 			}
-		} 
+			
+		
 		return "Auto original";
 	}
+
+	
 }
